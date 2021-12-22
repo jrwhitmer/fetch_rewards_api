50731e7e022aedcc1101e08f7e5a82e6140b8315
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-12-22 12:52:48 -0800

    Make bad request tests pass

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::TransactionsController < ApplicationController
   def create
-    transaction = Transaction.create!(transaction_params)
-    json_response(TransactionSerializer.new(transaction), :created)
+    if !params[:payer].present?
+      render_bad_request("Missing payer parameter in request")
+    elsif !params[:points].present?
+      render_bad_request("Missing points parameter in request")
+    elsif !params[:timestamp].present?
+      render_bad_request("Missing timestamp parameter in request")
+    else
+      transaction = Transaction.create!(transaction_params)
+      json_response(TransactionSerializer.new(transaction), :created)
+    end 
   end
 
   private
--- spec/requests/api/v1/transactions/new_spec.rb
+++ spec/requests/api/v1/transactions/new_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'POST /api/v1/transactions' do
 
     expect(response).to have_http_status(201)
   end
-  xit 'returns a 400 status when the request is made incorrectly' do
+  it 'returns a 400 status when the request is made incorrectly' do
     transaction_params = {
       payer: "DANNON",
       points: 500
@@ -23,7 +23,7 @@ RSpec.describe 'POST /api/v1/transactions' do
 
     expect(response).to have_http_status(400)
   end
-  xit 'returns an appropriate error message with the bad request' do
+  it 'returns an appropriate error message with the bad request' do
     transaction_params = {
       payer: "DANNON",
       points: 500
@@ -32,7 +32,7 @@ RSpec.describe 'POST /api/v1/transactions' do
 
     post '/api/v1/transactions', headers: headers, params: JSON.generate(transaction_params)
 
-    expect(response.body).to match(/Missing a parameter in request/)
+    expect(response.body).to match(/Missing timestamp parameter in request/)
   end
   xit 'creates a new transaction object when the request is successful' do
     transaction_params = {
