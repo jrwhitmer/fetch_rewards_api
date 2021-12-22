455af6b838bb4afa6299e563d45ec41e5c164064
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-12-22 11:47:51 -0800

    Add testing for validations within balance model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'webmock/rspec'
 require 'simplecov'
 SimpleCov.start do
   add_filter "app/controllers/api/v1/attendances_controller.rb"
@@ -67,7 +66,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include FactoryBot::Syntax::Methods
   end
 
   Shoulda::Matchers.configure do |config|
@@ -75,13 +73,4 @@ RSpec.configure do |config|
       with.test_framework :rspec
       with.library :rails
     end
-  end
-
-  VCR.configure do |config|
-    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-    config.hook_into :webmock
-    config.allow_http_connections_when_no_cassette = true
-    config.filter_sensitive_data('<api_key>') { ENV['pusher_key'] }
-    config.default_cassette_options = { re_record_interval: 7.days }
-    config.configure_rspec_metadata!
   end
